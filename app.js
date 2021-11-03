4b794dec5075cd8f6937fae380ab87580f41461b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-03 10:54:23 +0100

    middleware express

--- app.js
+++ app.js
@@ -1,59 +1,25 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+// req => middleware => res
+app.use([logger, authorize]);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+  res.send('Home');
 });
-
+app.get('/about', (req, res) => {
+  res.send('About');
+});
 app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
+  res.send('Products');
 });
-
-app.get('/api/products/:productID', (req, res) => {
-  //console.log(req.params);
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-
-  if (!singleProduct) {
-    return res.status(404).send('Produc Does Not Exist');
-  }
-
-  res.json(singleProduct);
-});
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params);
-  res.send('hello world');
-});
-
-app.get('/api/v1/query', (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) =>
-      product.name.startsWith(search)
-    );
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    //res.status(200).send('no products matched your search');
-    return res.status(200).json({ success: true, data: [] });
-  }
-  return res.status(200).json(sortedProducts);
+app.get('/api/items', (req, res) => {
+  console.log(req.user);
+  res.send('Items');
 });
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...');
+  console.log('Server is listening on port 5000...');
 });
